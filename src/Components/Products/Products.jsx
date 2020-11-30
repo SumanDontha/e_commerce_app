752c46a156d78f70f5b27af3c4ab168d470b73cb
Suman Dontha <dontha.suman@gmail.com>
2020-11-30 13:42:25 +0000

    Add to Cart functionlity.

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -9,7 +9,7 @@ import useStyles from './styles'
 //   { id: 2, name: "Mac Book Pro", description: "Apple Mac Book Laptop", price: '£10', image:'https://images.unsplash.com/photo-1588511986609-328d6ecae609?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8bWFjJTIwYm9vayUyMHByb3xlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=500&q=60'}
 // ]
 
-const Products = ({ products }) => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
   return (
     <main className={ classes.content }>
@@ -17,7 +17,7 @@ const Products = ({ products }) => {
       <Grid container justify="center" spacing={ 4 }>
         { products.map((product) => (
           <Grid item key={ product.id } xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 }>
-            <Product product={ product }/>
+            <Product product={ product } onAddToCart={ onAddToCart }/>
           </Grid>
         ))}
       </Grid>
